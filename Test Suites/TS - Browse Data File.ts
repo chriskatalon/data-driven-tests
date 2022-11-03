3272c5f3cd74e1d6abc497a65cf10c6a560ed2e0
chriskatalon <quan.trinh@katalon.com>
2022-11-03 16:35:37 +0700

    a

---
+++ Test Suites/TS - Browse Data File.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS - Browse Data File</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>5ffadd10-2ce3-4a40-9526-cf5afd3448fd</testSuiteGuid>
+   <testCaseLink>
+      <guid>9e734ca8-2efe-4903-b4c2-4355645a54c3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/BrowseExcelFile</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
